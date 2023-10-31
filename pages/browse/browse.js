8405cb62fe6d0212046a29e7c9d39138d2dc0ee7
megha-wadhwa12 <megha.wadhwa@kalvium.community>
2023-10-31 18:02:01 +0530

    navbar

---
+++ pages/browse/browse.js
@@ -0,0 +1,6 @@
+let onclickdrop = document.getElementById("dropdiv")
+let onclickflydiv = document.getElementById("onclickflydiv")
+onclickdrop.onclick=()=>{
+    // onclickflydiv.style.display = none;
+    onclickflydiv.classList.toggle('show')
+}
