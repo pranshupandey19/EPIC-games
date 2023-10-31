daf71c8af851def41407b07108d493dfe158be29
megha-wadhwa12 <megha.wadhwa@kalvium.community>
2023-10-31 18:09:28 +0530

    final-navbar

--- pages/browse/browse.js
+++ pages/browse/browse.js
@@ -4,3 +4,18 @@ onclickdrop.onclick=()=>{
     // onclickflydiv.style.display = none;
     onclickflydiv.classList.toggle('show')
 }
+
+
+
+
+const svgElement = document.querySelector('.reverse');
+let isRotated = false;
+
+svgElement.addEventListener('click', () => {
+  if (isRotated) {
+    svgElement.style.transform = 'rotate(0deg)';
+  } else {
+    svgElement.style.transform = 'rotate(180deg)';
+  }
+  isRotated = !isRotated;
+});
